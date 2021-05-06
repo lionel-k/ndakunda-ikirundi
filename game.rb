97359fb2c9fbaaa2fe0da3bf5cd64856f362b8fd
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2021-05-07 00:47:40 +0200

    fix removing key in dictionnary

--- game.rb
+++ game.rb
@@ -21,7 +21,7 @@ end
 
 def get_answers(guess)
   valid_answer = DICTIONARY[guess][LANGUAGE]
-  other_answers = DICTIONARY.tap { |h| h.delete(guess) }.values.map { |answer| answer[LANGUAGE] }
+  other_answers = DICTIONARY.dup.tap { |h| h.delete(guess) }.values.map { |answer| answer[LANGUAGE] }
   answers = [valid_answer].concat(other_answers.sample(4)).shuffle
   puts "answers"
   puts answers.map.with_index { |word, index| "#{index + 1}. #{word}"}.join("\n")
@@ -37,15 +37,19 @@ puts "Welcome"
 puts "----------"
 
 while running && rounds < total_rounds
-  puts ""
+  puts ''
   guess = get_guess
   answers = get_answers(guess)
   print "> "
   choice = gets.chomp.to_i
+  # puts choice = rand(1..5)
+  # puts choice = 5
+  # puts choice = (rounds % 5) + 1
   valid = valid_answer?(choice, answers)
   results += 1 if valid
   rounds += 1
   running = choice != 0
 end
 
-puts "Results: #{results} / #{rounds}"
+puts ''
+puts "Results: #{results * 100 / rounds} %"
