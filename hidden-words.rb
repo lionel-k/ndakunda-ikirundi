d6e6af62950b0b6747bb67531d6d02f3c561e7a2
Lionel Kubwimana <lionel.kubwimana@doctolib.com>
2021-12-11 18:30:08 +0100

    add word scramble

--- hidden-words.rb
+++ hidden-words.rb
@@ -2,8 +2,29 @@ require 'csv'
 
 table = CSV.parse(File.read('words.csv'), headers: true)
 
-DICTIONARY = table.map { |row| [ row['ki'], { fr: row['fr'], en: row['en'] } ] }.to_h
+DICTIONARY = table.map { |row| [ row['ki'].upcase, { fr: row['fr'], en: row['en'] } ] }.to_h
 KI_WORDS = DICTIONARY.keys
 
+def mix_letters(word)
+  word.upcase.chars.shuffle.join(' ')
+end
 
-puts KI_WORDS.sample(15)
+words = KI_WORDS.sample(5)
+puts words
+
+words.each do |word|
+  puts mix_letters(word)
+end
+
+
+# INGONA
+# IGITOKE
+# UMUKATE
+# INZOVU
+# AGAHUNGAREMA
+
+N A G O N I
+I T K E O I G
+K M A T E U U
+O N Z V U I
+G A E R U A A N M H G A
