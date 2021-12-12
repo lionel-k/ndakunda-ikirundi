25f943c55138eb7892ff62d4ffefee20f4a1c341
Lionel Kubwimana <lionel.kubwimana@doctolib.com>
2021-12-12 18:45:19 +0100

    add difficult words

--- hidden-words.rb
+++ hidden-words.rb
@@ -1,15 +1,17 @@
 require 'csv'
+require 'pry'
 
-table = CSV.parse(File.read('words.csv'), headers: true)
+simple = CSV.parse(File.read('simple-words.csv'), headers: true)
+difficult = CSV.parse(File.read('difficult-words.csv'), headers: true)
 
-DICTIONARY = table.map { |row| [ row['ki'].upcase, { fr: row['fr'], en: row['en'] } ] }.to_h
-KI_WORDS = DICTIONARY.keys
+# DICTIONARY = table.map { |row| [ row['ki'].upcase, { fr: row['fr'], en: row['en'] } ] }.to_h
+# KI_WORDS = DICTIONARY.keys
 
 def mix_letters(word)
   word.upcase.chars.shuffle.join(' ')
 end
 
-words = KI_WORDS.sample(5)
+words = difficult.to_a.flatten.select { |word| word.size > 5 }.sample(5)
 puts words
 
 words.each do |word|
@@ -23,8 +25,20 @@ end
 # INZOVU
 # AGAHUNGAREMA
 
-N A G O N I
-I T K E O I G
-K M A T E U U
-O N Z V U I
-G A E R U A A N M H G A
+# N A G O N I
+# I T K E O I G
+# K M A T E U U
+# O N Z V U I
+# G A E R U A A N M H G A
+
+
+
+
+IJAMBO
+UBWIZA
+KUGWIRA
+IMPWEMU
+UMUTWARE
+INYAMA
+UBWENGE
+
