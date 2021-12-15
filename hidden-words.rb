aed46f81236f4d5c8c048653237db92482deefec
Lionel Kubwimana <lionel.kubwimana@doctolib.com>
2021-12-15 23:34:03 +0100

    word search and scramble generator

--- hidden-words.rb
+++ hidden-words.rb
@@ -2,43 +2,35 @@ require 'csv'
 require 'pry'
 
 simple = CSV.parse(File.read('simple-words.csv'), headers: true)
-difficult = CSV.parse(File.read('difficult-words.csv'), headers: true)
+DICTIONARY = simple.map { |row| [ row['ki'].upcase, { fr: row['fr'], en: row['en'] } ] }.to_h
+simple_words = DICTIONARY.keys
 
-# DICTIONARY = table.map { |row| [ row['ki'].upcase, { fr: row['fr'], en: row['en'] } ] }.to_h
-# KI_WORDS = DICTIONARY.keys
+difficult_words = CSV.parse(File.read('difficult-words.csv'), headers: true).to_a
+
+WORDS = (simple_words + difficult_words).shuffle
 
 def mix_letters(word)
-  word.upcase.chars.shuffle.join(' ')
+  word.upcase.chars.shuffle.join('')
 end
 
-words = difficult.to_a.flatten.select { |word| word.size > 5 }.sample(5)
-puts words
-
-words.each do |word|
-  puts mix_letters(word)
+def words(min_size)
+  WORDS.select { |word| word.size > min_size }
 end
 
+def word_scramble(min_size:, size_sample:)
+  words = words(min_size).sample(size_sample)
+  puts words
+  puts '-' * 20
+  words.each do |word|
+    puts mix_letters(word)
+  end
+end
 
-# INGONA
-# IGITOKE
-# UMUKATE
-# INZOVU
-# AGAHUNGAREMA
+def word_search(min_size:, size_sample:)
+  words = words(min_size).sample(size_sample)
+  puts words
+end
 
-# N A G O N I
-# I T K E O I G
-# K M A T E U U
-# O N Z V U I
-# G A E R U A A N M H G A
-
-
-
-
-IJAMBO
-UBWIZA
-KUGWIRA
-IMPWEMU
-UMUTWARE
-INYAMA
-UBWENGE
+word_scramble(min_size: 8, size_sample: 4)
+word_search(min_size: 5, size_sample: 10)
 
