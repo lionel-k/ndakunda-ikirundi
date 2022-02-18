fa505ab56b2c0e238c8cc3081fb16e1f0b5411fc
Lionel Kubwimana <lionel.kubwimana@doctolib.com>
2022-02-18 23:51:28 +0100

    improve

--- hidden-words.rb
+++ hidden-words.rb
@@ -3,11 +3,11 @@ require 'pry'
 
 simple = CSV.parse(File.read('simple-words.csv'), headers: true)
 DICTIONARY = simple.map { |row| [ row['ki'].upcase, { fr: row['fr'], en: row['en'] } ] }.to_h
-simple_words = DICTIONARY.keys
+SIMPLE_WORDS = DICTIONARY.keys
 
-difficult_words = CSV.parse(File.read('difficult-words.csv'), headers: true).to_a
+DIFFICULT_WORDS = CSV.parse(File.read('difficult-words.csv'), headers: true).to_a
 
-WORDS = (simple_words + difficult_words).shuffle
+WORDS = (SIMPLE_WORDS + DIFFICULT_WORDS).shuffle
 
 def mix_letters(word)
   word.upcase.chars.shuffle.join('')
@@ -31,6 +31,14 @@ def word_search(min_size:, size_sample:)
   puts words
 end
 
-word_scramble(min_size: 8, size_sample: 4)
-word_search(min_size: 5, size_sample: 10)
+def word_match
+  SIMPLE_WORDS.sample(4).each do |word|
+    puts word
+    puts SIMPLE_WORDS.sample(2)
+    puts '-' * 20
+  end
+end
 
+# word_scramble(min_size: 8, size_sample: 4)
+# word_search(min_size: 5, size_sample: 7)
+word_match
